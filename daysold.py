80b052eb8f375f6b695556b3e8a89f1155e78a37
Vivek Vishwakarma <viveksallu156@gmail.com>
2017-07-16 15:28:54 +0530

    small coding by importing time and date library

---
+++ daysold.py
@@ -0,0 +1,15 @@
+import time #imports time library
+
+from datetime import date
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    
+    today = date(year2, month2, day2)
+    
+    bDay = date(year1, month1, day1)
+    
+    time_to_today = abs(bDay - today)
+    
+    return time_to_today.days
+
+print(daysBetweenDates(1992, 11, 20, 2017, 7, 7))
